f83894718aecd64ebd027ad6297eca28820d9866
Anton <stepasha587@mail.ru>
2021-03-05 16:00:36 +0300

    create menu nav component

--- front/src/components/Banner/Banner.js
+++ front/src/components/Banner/Banner.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import styles from './Banner.module.scss'
 
 function Banner(props) {
     return (
         <div>
-            <section id="banner" className={styles.major}>
+            <section id="banner" className="major">
                     <div className="inner">
-                        <header className={styles.major}>
-                            <h1>Hi, my name is Kirusha</h1>
+                        <header className="major">
+                            <h1>Hi, my name is MCT</h1>
                         </header>
-                        <div className={styles.context}>
+                        <div className="content">
                             <p>A responsive site template designed by HTML5 UP<br />
                             and released under the Creative Commons.</p>
-                            <ul className={styles.actions}>
+                            <ul className="actions">
                                 <li><a href="#one" className="button next scrolly">Get Started</a></li>
                             </ul>
                         </div>
---
+++ front/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <header id="header">
+      <nav>
+        <a href="#menu">Menu</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
