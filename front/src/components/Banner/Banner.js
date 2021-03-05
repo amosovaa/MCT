dc014b2e04a1c685592bd430960ae4eea97d1730
Anton <stepasha587@mail.ru>
2021-03-05 15:52:39 +0300

    Update banner component

--- front/src/components/Banner/Banner.js
+++ front/src/components/Banner/Banner.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import styles from './Banner.module.scss'
 
 function Banner(props) {
     return (
         <div>
-            <section id="banner" class="major">
-                    <div class="inner">
-                        <header class="major">
+            <section id="banner" className={styles.major}>
+                    <div className="inner">
+                        <header className={styles.major}>
                             <h1>Hi, my name is Kirusha</h1>
                         </header>
-                        <div class="content">
+                        <div className={styles.context}>
                             <p>A responsive site template designed by HTML5 UP<br />
                             and released under the Creative Commons.</p>
-                            <ul class="actions">
-                                <li><a href="#one" class="button next scrolly">Get Started</a></li>
+                            <ul className={styles.actions}>
+                                <li><a href="#one" className="button next scrolly">Get Started</a></li>
                             </ul>
                         </div>
                     </div>
