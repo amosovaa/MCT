a8dd514c8db199fe871d58e6d2420250a3c65a65
Polonsky13 <a.polonsky13@yandex.ru>
2021-03-06 14:08:11 +0300

    name adminCabinet

--- front/src/components/AdminCabinet/AdminCabinet.js
+++ front/src/components/AdminCabinet/AdminCabinet.js
@@ -11,55 +11,55 @@ function AdminCabinet(props) {
         <fieldset>
           <legend>Город</legend>
           <label>
-            <input type='text' placeholder='Город' />
+            <input type='text' placeholder='Город' name='cityName' />
           </label>
         </fieldset>
         <fieldset>
           <legend>Отель</legend>
           <label>
-            <input type='text' placeholder='Название отеля' />
+            <input type='text' placeholder='Название отеля' name='hotelName'/>
           </label>
           <label>
-            <input type='date' placeholder='Дата приезда' />
+            <input type='date' placeholder='Дата приезда' name='dateIn' />
           </label>
           <label>
-            <input type='time' placeholder='Время приезда' />
+            <input type='time' placeholder='Время приезда' name='timeIn'/>
           </label>
           <label>
-            <input type='date' placeholder='Дата выезда' />
+            <input type='date' placeholder='Дата выезда' name='dateOut'/>
           </label>
           <label>
-            <input type='time' placeholder='Время выезда' />
+            <input type='time' placeholder='Время выезда' name='timeOut'/>
           </label>
           <label>
-            <input type='text' placeholder='Адрес отеля' />
+            <input type='text' placeholder='Адрес отеля' name='hotelAddress'/>
           </label>
         </fieldset>
         <fieldset>
           <legend>Концертный зал</legend>
           <label>
-            <input type='text' placeholder='Адрес концертного зала' />
+            <input type='text' placeholder='Адрес концертного зала' name='hallAddress' />
           </label>
           <label>
-            <input type='time' placeholder='Время начала репетиции' />
+            <input type='time' placeholder='Время начала репетиции' name='timeRepetition'/>
           </label>
           <label>
-            <input type='time' placeholder='Время конца репетиции' />
+            <input type='time' placeholder='Время конца репетиции' name='timeRepetitionEnd' />
           </label>
           <label>
-            <input type='time' placeholder='Время начала первого концерта' />
+            <input type='time' placeholder='Время начала первого концерта' name='timeConcert' />
           </label>
           <label>
-            <input type='time' placeholder='Время начала второго концерта' />
+            <input type='time' placeholder='Время начала второго концерта' name='timeConcertSecond'/>
           </label>
         </fieldset>
         <fieldset>
           <legend>Еда</legend>
           <label>
-            <input type='text' placeholder='Адрес еды' />
+            <input type='text' placeholder='Адрес еды' name='addressLunch' />
           </label>
           <label>
-            <input type='time' placeholder='Время еды' />
+            <input type='time' placeholder='Время еды' name='timeLunch'/>
           </label>
         </fieldset>
         <button> Создать город </button>
