446930ddd9659915ef1a3f586d308a8519fc760e
amosovaa <asya.amosova@yandex.ru>
2021-03-06 14:01:18 +0300

    main cabinet page

--- front/src/components/Cabinet/Cabinet.js
+++ front/src/components/Cabinet/Cabinet.js
@@ -16,9 +16,29 @@ function Cabinet(props) {
       <br />
       <br />
       <br />
-      <Link to='/cabinets/schedule'>Общее расписание</Link>
+      <Link to='/cabinets/schedule'>
+        <section id='one' className='tiles'>
+          <article style={{ cursor: 'pointer' }}>
+            <span className='image'></span>
+            <header className='major'>
+              <h3>Расписание</h3>
+              <p>Узнать расписание на день</p>
+            </header>
+          </article>
+        </section>
+      </Link>
       <br />
-      <Link to='/cabinets/admincabinet'>Админская штука</Link>
+      <Link to='/cabinets/admincabinet'>
+        <section id='one' className='tiles'>
+          <article style={{ backgroundColor: 'green', cursor: 'pointer' }}>
+            <span className='image'></span>
+            <header className='major'>
+              <h3>Создать расписание</h3>
+              <p>Создать расписание для нового города</p>
+            </header>
+          </article>
+        </section>
+      </Link>
     </div>
   );
 }
