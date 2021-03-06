d7c5d3f5bb3cc0713acc299cdcded74f52969f32
amosovaa <asya.amosova@yandex.ru>
2021-03-06 13:31:56 +0300

    admin cabinet component

--- front/src/components/Banner/Banner.js
+++ front/src/components/Banner/Banner.js
@@ -1,24 +1,31 @@
 import React from 'react';
 
 function Banner(props) {
-    return (
-        <div>
-            <section id="banner" className="major">
-                    <div className="inner">
-                        <header className="major">
-                            <h1>Hi, my name is MCT</h1>
-                        </header>
-                        <div className="content">
-                            <p>A responsive site template designed by HTML5 UP<br />
-                            and released under the Creative Commons.</p>
-                            <ul className="actions">
-                                <li><a href="#one" className="button next scrolly">Get Started</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
+  return (
+    <div>
+      <section id='banner' className='major'>
+        <div className='inner'>
+          <header className='major'>
+            <h1>Hi, welcome to «My Concert Tour»</h1>
+          </header>
+          <div className='content'>
+            <p>
+              Create your own tour for an orchestra
+              <br />
+              or touristic group.
+            </p>
+            <ul className='actions'>
+              <li>
+                <a href='#one' className='button next scrolly'>
+                  Get Started
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 }
 
 export default Banner;
