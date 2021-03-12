7a25a1f648e645dc11af83e4897e52047e148cba
inothing <inothing1087@gmail.com>
2021-03-12 12:29:30 +0300

    compass

--- front/src/components/Compass/Compass.js
+++ front/src/components/Compass/Compass.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { COORD_BUS } from "../../redux/actionTypes";
-
+import styles from './compass.module.scss'
 function Compass(props) {
 
   const dispatch = useDispatch();
 
 
   return (
-      <button onClick={()=>{
+      <span className={styles.wrapper} onClick={()=>{
         navigator.geolocation.getCurrentPosition((position) =>{
           dispatch({type:COORD_BUS, payload: {lat:position.coords.latitude, lng: position.coords.longitude} })
         }, ()=> null)
       }}>
-        <img src="compass.svg" alt="locate me" style={{width:"64px", height:'64px'}}></img>
-      </button>
+        <img src="/compass.svg" alt="locate me" style={{width:"48px", height:'48px'}}></img>
+      </span>
   );
 }
 
